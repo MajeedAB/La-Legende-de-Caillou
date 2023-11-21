42d583a21d4a7b48b46451c9be3b55408d20a200
MajeedAB <majeedbaki@outlook.com>
2023-11-21 17:50:07 -0500

    TEMP: ne fonctionne pas mais debut creation niveaux

--- LevelAssets.py
+++ LevelAssets.py
@@ -7,21 +7,38 @@ def get_level_background(level):
 def get_treasure_sprite(level):
     return load_image('treasures/treasure' + str(level) + '.png')
 
+def get_tile_sprites():
+    return [
+        load_image('platforms/p0.png'),
+        load_image('platforms/p1.png'),
+        load_image('platforms/p2.png'),
+        load_image('platforms/p3.png'),
+        load_image('platforms/p4.png'),
+        load_image('platforms/p5.png'),
+        # load_image('platforms/p6.png'),
+        # load_image('platforms/p7.png'),
+        # load_image('platforms/p8.png'),
+        # load_image('platforms/p9.png'),
+        # load_image('platforms/p10.png'),
+        # load_image('platforms/p11.png'),
+        # load_image('platforms/p12.png'),
+        ]
+
 caillou_start_pos = [
-    (250, 200),
-    (400, 200),
-    (700, 350),
-    (700, 350)
+    (0, 0),
+    (0, 0),
+    (0, 0),
+    (0, 0)
 ]
 
 def get_caillou_start_pos(level):
     return caillou_start_pos[level-1]
 
 treasure_coords = [
-    [500, 500],
-    [500, 600],
-    [300, 700],
-    [500, 700]
+    [250, 150],
+    [100, 100],
+    [100, 100],
+    [100, 100]
 ]
 
 def get_treasure_coords(level):
