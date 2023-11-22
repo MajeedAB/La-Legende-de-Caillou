c35038e724a857bc3b36ee3054e9d89ce9d8abdd
VictorGilbert <zevictos@gmail.com>
2023-11-21 22:17:06 -0500

    Ajout de la 3eme cinématique

--- Cinematique.py
+++ Cinematique.py
@@ -4,10 +4,12 @@ import pygame
 NUMBER_OF_IMAGES_CUTSCENE_0 = 14
 NUMBER_OF_IMAGES_CUTSCENE_1 = 9
 NUMBER_OF_IMAGES_CUTSCENE_2 = 10
+NUMBER_OF_IMAGES_CUTSCENE_3 = 11
 
 DIALOG_PATH_SPYRO =  'Dialogue_Spyro/Dialogue'
 DIALOG_PATH_DRMARIO = 'Dialogue_DrMario/Dialogue'
 DIALOG_PATH_PIKACHU =  'Dialogue_Pikachu/Dialogue'
+DIALOG_PATH_OLIMAR= 'Dialogue_Olimar/Dialogue'
 DIALOG_EXT = '.png'
 
 
@@ -31,6 +33,14 @@ def cinematique_1(screen, display, clock):
 def cinematique_2(screen, display, clock):
     for i in range (NUMBER_OF_IMAGES_CUTSCENE_2):
         img = pygame.image.load(DIALOG_PATH_PIKACHU + str(i) + DIALOG_EXT).convert()
+        img.set_colorkey((45, 73, 255))
+        screen.blit(pygame.transform.scale(img, screen.get_size()), (0, 0))
+        pygame.display.update()
+        clock.tick(0.5)
+
+def cinematique_3(screen, display, clock):
+    for i in range (NUMBER_OF_IMAGES_CUTSCENE_3):
+        img = pygame.image.load(DIALOG_PATH_OLIMAR + str(i) + DIALOG_EXT).convert()
         img.set_colorkey((45, 73, 255))
         screen.blit(pygame.transform.scale(img, screen.get_size()), (0, 0))
         pygame.display.update()
