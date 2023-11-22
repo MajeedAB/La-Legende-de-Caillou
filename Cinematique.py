4fe0a70b3620517a3ae7ecf9e64a092a879d6397
MajeedAB <majeedbaki@outlook.com>
2023-11-21 23:57:41 -0500

    cinematique fin

--- Cinematique.py
+++ Cinematique.py
@@ -5,11 +5,13 @@ NUMBER_OF_IMAGES_CUTSCENE_0 = 14
 NUMBER_OF_IMAGES_CUTSCENE_1 = 9
 NUMBER_OF_IMAGES_CUTSCENE_2 = 10
 NUMBER_OF_IMAGES_CUTSCENE_3 = 11
+NUMBER_OF_IMAGES_CUTSCENE_4 = 9
 
 DIALOG_PATH_SPYRO =  'Dialogue_Spyro/Dialogue'
 DIALOG_PATH_DRMARIO = 'Dialogue_DrMario/Dialogue'
 DIALOG_PATH_PIKACHU =  'Dialogue_Pikachu/Dialogue'
 DIALOG_PATH_OLIMAR= 'Dialogue_Olimar/Dialogue'
+DIALOG_PATH_FIN= 'Dialogue_Fin/Dialogue'
 DIALOG_EXT = '.png'
 
 
@@ -41,6 +43,14 @@ def cinematique_2(screen, display, clock):
 def cinematique_3(screen, display, clock):
     for i in range (NUMBER_OF_IMAGES_CUTSCENE_3):
         img = pygame.image.load(DIALOG_PATH_OLIMAR + str(i) + DIALOG_EXT).convert()
+        img.set_colorkey((45, 73, 255))
+        screen.blit(pygame.transform.scale(img, screen.get_size()), (0, 0))
+        pygame.display.update()
+        clock.tick(0.35)
+
+def cinematique_4(screen, display, clock):
+    for i in range (NUMBER_OF_IMAGES_CUTSCENE_4):
+        img = pygame.image.load(DIALOG_PATH_FIN + str(i) + DIALOG_EXT).convert()
         img.set_colorkey((45, 73, 255))
         screen.blit(pygame.transform.scale(img, screen.get_size()), (0, 0))
         pygame.display.update()
